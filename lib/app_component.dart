8bb39b154a113d9f1c333ad56cb44fa48cb85bfd
Burak İnce <burak.ince@linux.org.tr>
2015-12-06 19:47:49 +0200

    ADDED: Angular 2 initial project added.

---
+++ lib/app_component.dart
@@ -0,0 +1,9 @@
+library free_poll.app_component;
+
+import 'package:angular2/angular2.dart';
+
+//@Component(selector: 'my-app', templateUrl: 'app_component.html')
+//@Component(selector: 'my-app', template: '<h2>Hello World</h2>')
+@Component(selector: 'my-app')
+@View(template: '<h1>My first Angular 2 App</h1>')
+class AppComponent {}
---
+++ web/main.dart
@@ -0,0 +1,9 @@
+library free_poll.web;
+
+import 'package:angular2/bootstrap.dart';
+
+import 'package:free_poll/app_component.dart';
+
+main() {
+  bootstrap(AppComponent);
+}
