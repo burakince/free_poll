ce614d96304fcba8aa565583053745e0b3e4597a
Burak İnce <burak.ince@linux.org.tr>
2015-12-06 20:17:49 +0200

    ADDED: Static list component added.

--- web/main.dart
+++ web/main.dart
@@ -5,9 +5,11 @@ import 'package:angular2/bootstrap.dart';
 import 'package:free_poll/app_component.dart';
 import 'package:free_poll/my_name_component.dart';
 import 'package:free_poll/time_component.dart';
+import 'package:free_poll/static_list_component.dart';
 
 main() {
   bootstrap(AppComponent);
   bootstrap(MyNameComponent);
   bootstrap(TimeComponent);
+  bootstrap(StaticListComponent);
 }
